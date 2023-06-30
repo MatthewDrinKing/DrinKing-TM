646b390a257eef00178cdaebc5241e0a62e7388b
freddie <matthew@drinking.co.uk>
2023-06-30 14:29:26 +0100

    updating default ports

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post("/api/receivePrintJob", function (req, res) {
 
 app.use(express.static("static"));
 
-var server = app.listen(8080, function () {
+var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
